054e6230bea9c5268f99f6e7b7197072ba8bcf54
Ivan Kelembetov <ikelembetov@zeuselectronics.eu>
2015-05-19 15:05:58 +0300

    first commit

---
+++ Arr.php
@@ -0,0 +1,63 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Arr extends Kohana_Arr {
+
+    public static function get($array, $key, $default = NULL) {
+        if (strpos($key, '=>') !== false) {
+            return Arr::get_multidimensional($array, $key, $default);
+        }
+        return parent::get($array, $key, $default);
+    }
+
+    /**
+     * Get data by case-insensitive key
+     * @param $array
+     * @param $key
+     * @param null $default
+     * @return mixed
+     */
+    public static function geti($array, $key, $default = NULL)
+    {
+        if (strpos($key, '=>') !== false) {
+            return Arr::geti_multidimensional($array, $key, $default);
+        }
+        foreach ($array as $k => $v) {
+            if (strtolower($k) == strtolower($key)) {
+                return $v;
+            }
+        }
+        return $default;
+    }
+
+    /**
+     * Get data from multi-dimension array
+     * @param $array - Source array
+     * @param $keys - Multi-dimension key. String like: "parent=>child1=>child2"
+     * @param $default - Default value
+     * @return mixed
+     */
+    protected static function get_multidimensional($array, $keys, $default = NULL) {
+        $keys_arr = explode('=>', $keys);
+        $value = $array;
+        foreach ($keys_arr as $key) {
+            $value = parent::get($value, $key, $default);
+        }
+        return $value;
+    }
+
+    /**
+     * Get data from multi-dimension array by case-insensitive key
+     * @param $array - Source array
+     * @param $keys - Multi-dimension key. String like: "parent=>child1=>child2"
+     * @param $default - Default value
+     * @return mixed
+     */
+    protected static function geti_multidimensional($array, $keys, $default = NULL) {
+        $keys_arr = explode('=>', $keys);
+        $value = $array;
+        foreach ($keys_arr as $key) {
+            $value = Arr::geti($value, $key, $default);
+        }
+        return $value;
+    }
+}
